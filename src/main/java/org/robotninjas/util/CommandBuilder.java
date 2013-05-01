d31ab3ddf0c42fec56fcd3badddd3152c42b9b12
Dave Rusek <dave.rusek@gmail.com>
2013-05-01 10:49:20 -0600

    Actually in actuality adding Retryer to CommandBuilder

--- src/main/java/org/robotninjas/util/CommandBuilder.java
+++ src/main/java/org/robotninjas/util/CommandBuilder.java
@@ -1,17 +1,19 @@
 package org.robotninjas.util;
 
+import com.github.rholder.retry.Retryer;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
-import com.google.common.util.concurrent.*;
+import com.google.common.util.concurrent.FutureFallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.SimpleTimeLimiter;
+import com.google.common.util.concurrent.TimeLimiter;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
-import static com.google.common.util.concurrent.MoreExecutors.listeningDecorator;
 import static com.google.common.util.concurrent.MoreExecutors.sameThreadExecutor;
 
 public class CommandBuilder<V> {
@@ -22,6 +24,7 @@ public class CommandBuilder<V> {
   private Optional<TimeUnit> unit = Optional.absent();
   private Optional<Long> duration = Optional.absent();
   private Optional<FutureFallback<V>> fallback = Optional.absent();
+  private Optional<Retryer<V>> retryer = Optional.absent();
 
   @VisibleForTesting
   CommandBuilder(TimeLimiter limiter) {
@@ -32,28 +35,33 @@ public class CommandBuilder<V> {
     this(new SimpleTimeLimiter());
   }
 
-  public static CommandBuilder newBuilder() {
+  public static <V> CommandBuilder<V> newBuilder() {
     return new CommandBuilder();
   }
 
-  public CommandBuilder call(Callable<V> callable) {
+  public CommandBuilder<V> call(Callable<V> callable) {
     this.callable = Optional.of(checkNotNull(callable));
     return this;
   }
 
-  public CommandBuilder within(long duration, TimeUnit unit) {
+  public CommandBuilder<V> within(long duration, TimeUnit unit) {
     this.unit = Optional.of(checkNotNull(unit));
     this.duration = Optional.of(checkNotNull(duration));
     return this;
   }
 
-  public CommandBuilder withFallback(FutureFallback<V> fallback) {
+  public CommandBuilder<V> withFallback(FutureFallback<V> fallback) {
     this.fallback = Optional.of(checkNotNull(fallback));
     return this;
   }
 
-  public CommandBuilder onExecutor(Executor executor) {
+  public CommandBuilder<V> onExecutor(Executor executor) {
     this.executor = Optional.of(checkNotNull(executor));
+    return this;
+  }
+
+  public CommandBuilder<V> withRetryer(Retryer<V> retryer) {
+    this.retryer = Optional.of(retryer);
     return this;
   }
 
@@ -71,6 +79,10 @@ public class CommandBuilder<V> {
 
     if (fallback.isPresent()) {
       caller.setFallback(fallback.get());
+    }
+
+    if (retryer.isPresent()) {
+      caller.setRetryer(retryer.get());
     }
 
     return caller;
